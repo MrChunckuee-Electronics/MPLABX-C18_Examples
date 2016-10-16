41df7504dc135200a0a7b437dbebc77b6bf07ff1
Pedro Sánchez <mrchunckuee.psr@gmail.com>
2016-10-16 14:28:44 -0500

    Interrupts library

---
+++ Librerias/all_interrupts.h
@@ -0,0 +1,15 @@
+/*
+ * File:   all_interrupts.h
+ * Author: MrChunckuee
+ * Configuración de FUSES del PIC18F4620
+ * Created on 26 de diciembre de 2014, 12:57 AM
+ */
+
+#ifndef ALL_INTERRUPTS_H
+#define ALL_INTERRUPTS_H
+
+//Funciones utilizadas en las interrupciones
+void low_isr(void);
+void high_isr(void);
+
+#endif
